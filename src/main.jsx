c0b486bd1f92ae309c6196c96d074fc2f4d57913
shimaa2468 <shamazourob@gmail.com>
2025-03-04 03:46:59 -0800

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import { I18nextProvider, useTranslation } from 'react-i18next';
 import './../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
 import "./i18n.js";
+import i18n from "./i18n"; // تأكد من وجود ملف i18n.js في المشروع
+
+function TitleUpdater() {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    document.title = t("app.title"); // تحديث العنوان عند تغيير اللغة
+  }, [i18n.language]);
+
+  return null; // هذا الكومبوننت لا يعرض شيئًا
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <TitleUpdater />
+      <App />
+    </I18nextProvider>
   </StrictMode>,
-)
+);
